c92a5a5511a0234f4452ac3c51727e52c44996db
mbrmma <mamaevbekmyrza9@gmail.com>
2026-04-28 10:24:27 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,39 @@
+import asyncio
+from aiogram import Bot, Dispatcher, types
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
+TOKEN = "7250624876:AAFxaw6i7JS6gD0n0F7uasFEXk6OcKmLn1Y"
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+kb = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton(text="🍓 Купить клубнику")],
+        [KeyboardButton(text="📦 Мой заказ")]
+    ],
+    resize_keyboard=True
+)
+
+@dp.message()
+async def start(message: types.Message):
+    if message.text == "/start":
+        await message.answer("Добро пожаловать 🍓\nВыберите действие:", reply_markup=kb)
+
+    elif message.text == "🍓 Купить клубнику":
+        await message.answer(
+            "Выберите фасовку:\n\n"
+            "0.5 кг — 500₽\n"
+            "1 кг — 1000₽\n"
+            "1.5 кг — 1500₽\n"
+            "2 кг — 2000₽"
+        )
+
+    elif message.text == "📦 Мой заказ":
+        await message.answer("У вас пока нет заказов 😔")
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
